38dda324a78db875b160fba007a117b15b6555f4
Jonathan Beard <jbeard@wustl.edu>
2013-12-10 15:24:26 -0600

    updates

--- ASTNodes/ValueBase.hpp
+++ ASTNodes/ValueBase.hpp
@@ -12,7 +12,12 @@ class ValueBase : public NodeAbstract {
 public:
    ValueBase() = default;
    virtual ~ValueBase() = default;
-
+   
+   /**
+    * get_value - To get a value that this object was constructed
+    * with, pass an initialized ptr (initialized with the proper
+    * length for that data type) 
+    */
    virtual bool get_value( const char *typeName, void *ptr ) = 0;
 };
 } /* end namespace Node */
