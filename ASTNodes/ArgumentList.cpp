9fd1600abf44a5e537e1a07f9389e39ca539c9dd
jonathan-beard <jonathan.c.beard@gmail.com>
2013-12-29 13:32:32 -0600

    updates

--- ASTNodes/ArgumentList.cpp
+++ ASTNodes/ArgumentList.cpp
@@ -4,3 +4,16 @@
  * @version: Sun Dec 29 13:17:31 2013
  */
 #include "ArgumentList.hpp"
+
+using namespace Node;
+
+ArgumentList::ArgumentList() : NodeAbstract( "ArgumentList" )
+{
+   /** nothing to do here **/
+}
+
+ArgumentList::ArgumentList( const std::string name ) : 
+                              NodeAbstract( name )
+{
+   /** nothing to do here **/
+}
--- ASTNodes/StreamArgumentList.hpp
+++ ASTNodes/StreamArgumentList.hpp
@@ -5,5 +5,14 @@
  */
 #ifndef _STREAMARGUMENTLIST_HPP_
 #define _STREAMARGUMENTLIST_HPP_  1
-
+#include "ArgumentList.hpp"
+namespace Node{
+class StreamArgumentList : public ArgumentList {
+public:
+   StreamArgumentList();
+   virtual ~StreamArgumentList() = default;
+protected:  
+   StreamArgumentList( const std::string name );
+};
+} /** end namespace Node **/
 #endif /* END _STREAMARGUMENTLIST_HPP_ */
