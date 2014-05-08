7591514e072685696a560f3597b1d79fd674c60c
jonathan-beard <jonathan.c.beard@gmail.com>
2014-05-08 09:37:09 -0500

    updates

--- symtab.hpp
+++ symtab.hpp
@@ -27,11 +27,46 @@ public:
    SymTab();
    virtual ~SymTab();
 
+   /**
+    * addType - since the language allows for multiple declarations 
+    * with a single type declaration, the type is added first then
+    * multiple symbols can be added for the type.  The type is stored 
+    * internally.
+    * @param   type  - const std::string
+    */
+   void  addType(  const std::string type  );
+   /**
+    * addScope - increments the scope with the given name.  The name
+    * is used in the hashing function for the symbol.
+    * @param   scope - const std::string
+    */
    void  addScope( const std::string scope );
+   /**
+    * addAnonymousScope - increments an anonymous scope, i.e., a block
+    * with curly braces that is used to identify a scoped section that
+    * simply has no name.  This scope is assigned a random string of
+    * characters to identify it.
+    */
    void  addAnonymousScope();
+   /**
+    * addSymbol - adds the symbol within the currently active scope and
+    * with the associated current type.
+    * @param   symbol - const std::string
+    */
    void  addSymbol( const std::string symbol );
-
+   /**
+    * leaveScope - decrements the current scope.
+    */
    void  leaveScope();
+   /**
+    * getTypeForSymbol - gets the type for the symbol currently
+    * defined in this scope.
+    * @param   symbol - const std::string
+    * @param   type   - std::string&, symbol is stored here if function
+    *                   returns true
+    * @return  bool   - true if symbol exists, false if not
+    */
+    bool getTypeForSymbol( const std::string symbol );
 
 };
 }
